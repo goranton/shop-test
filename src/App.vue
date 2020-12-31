c7ae08a735fd68c245453d9363e68401bc14ec3a
anton <anton@cake.ru>
2020-12-31 08:28:05 +0300

    Remove debug information from App component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,5 @@
 <template>
   <div id="app">
-    <block-expansion header="test" expanded>
-      <template v-slot:default="{ isExpanded }">
-        {{ isExpanded }}
-      </template>
-    </block-expansion>
-    <field-counter v-model="counter" />
-    <grid :items="[1, 2, 3, 4, 5]">
-      <template v-slot:columns="{ columns }">
-        <grid-column v-for="{ id, items } in columns" :key="id">
-          {{ items }}
-        </grid-column>
-      </template>
-    </grid>
     <router-view />
   </div>
 </template>
@@ -22,16 +9,7 @@
 </style>
 
 <script>
-import Grid from "./components/grid";
-import GridColumn from "./components/grid-column";
-import FieldCounter from "./components/field-counter";
-import BlockExpansion from "./components/block-expansion";
 export default {
-  components: { BlockExpansion, FieldCounter, GridColumn, Grid },
-  data() {
-    return {
-      counter: 1
-    };
-  }
+  name: "Application"
 };
 </script>
