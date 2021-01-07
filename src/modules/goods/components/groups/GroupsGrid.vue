4f68fd50e46ea9dc2cc15e013ebc3da32f88f8b0
anton <anton@cake.ru>
2021-01-07 19:08:07 +0300

    groups grid component

---
+++ src/modules/goods/components/groups/GroupsGrid.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="groups-grid">
+    <grid :items="groups">
+      <template v-slot:columns="{ columns }">
+        <grid-column v-for="{ id, items } in columns" :key="id">
+          <block-expansion
+            v-for="group in items"
+            :key="group.id"
+            :header="group.name"
+            expanded
+          >
+            <template v-slot:default>
+              <goods-inline-list>
+                <good-inline
+                  v-for="item in getItems(group.id)"
+                  :key="item.id"
+                  :title="item.name"
+                  :price="item.price"
+                />
+              </goods-inline-list>
+            </template>
+          </block-expansion>
+        </grid-column>
+      </template>
+    </grid>
+  </div>
+</template>
+
+<script>
+import Grid from "../../../../components/grid";
+import GridColumn from "../../../../components/grid-column";
+import BlockExpansion from "../../../../components/block-expansion";
+import GoodsInlineList from "../goods/GoodsInlineList";
+import GoodInline from "../goods/GoodInline";
+export default {
+  props: {
+    groups: {
+      type: Array,
+      default: () => []
+    },
+    getItems: {
+      type: Function,
+      default: () => () => {}
+    }
+  },
+  name: "GroupsGrid",
+  components: { GoodInline, GoodsInlineList, BlockExpansion, GridColumn, Grid }
+};
+</script>
+
+<style scoped></style>
--- src/modules/goods/pages/Index.vue
+++ src/modules/goods/pages/Index.vue
@@ -1,14 +1,21 @@
 <template>
   <div>
-    <grid items=""></grid>
+    <groups-grid :groups="groups" :get-items="getItems" />
   </div>
 </template>
 
 <script>
-import Grid from "../../../components/grid";
+import { mapGetters } from "vuex";
+import { GETTERS } from "../store/constants";
+import GroupsGrid from "../components/groups/GroupsGrid";
+
 export default {
   name: "GoodsIndexPage",
-  components: { Grid }
+  components: { GroupsGrid },
+  computed: mapGetters("goods", {
+    groups: GETTERS.GET_GROUPS,
+    getItems: GETTERS.GET_ITEMS_FOR_GROUP
+  })
 };
 </script>
 
